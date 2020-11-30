05c3015b618e6e752cffcdb06717497e355a6efe
quantimphysics <timothyhodge14@gmail.com>
2020-11-30 13:31:26 -0800

    new background hehe

---
+++ src/main/java/com/example/adoptafossil/AdoptafossilApplication.java
@@ -0,0 +1,13 @@
+package com.example.adoptafossil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptafossilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoptafossilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/adoptafossil/AdoptafossilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adoptafossil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptafossilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
