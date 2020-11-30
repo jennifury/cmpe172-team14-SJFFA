4bd394ade5d7e5a7bdc811cf870c9bcb636f186f
Terry <tgmyers5@gmail.com>
2020-11-29 19:31:40 -0800

    login button says nice based on input

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function Login(){
-
+  if(document.forms["login"].username.value == "hi"
+  && document.forms["login"].password.value == "nice"){
+    alert("nice")
+  } else
+    alert("Please try again")
 }
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <form id="login">
 
         Welcome to the San Jose Fictional Fossil Archive
         <br></br><br></br>
@@ -18,6 +21,8 @@ function App() {
         <i>Don't remember? Talk to your supervisor.</i>
         <br></br><br></br>
 
+        <form id="login">
+
         <label for="username">Username: </label>
         <input type="text" id="username"/>
         <br></br>
@@ -25,7 +30,7 @@ function App() {
         <input type="text" id="password"/>
         
         <br></br>
-        <button type="button" onClick="Login()">Log in</button>
+        <button type="button" onClick={Login}>Log in</button>
         
         </form>
       </header>
