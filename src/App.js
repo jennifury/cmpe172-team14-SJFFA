6ad18361d718cfc3bdbe3a194756c9c97677e080
Terry <tgmyers5@gmail.com>
2020-11-29 19:14:45 -0800

    login mockup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Login(){
+
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form id="login">
+
+        Welcome to the San Jose Fictional Fossil Archive
+        <br></br><br></br>
+        Please log in with your employee credentials.
+        <br></br>
+        <i>Don't remember? Talk to your supervisor.</i>
+        <br></br><br></br>
+
+        <label for="username">Username: </label>
+        <input type="text" id="username"/>
+        <br></br>
+        <label for="password">Password: </label>
+        <input type="text" id="password"/>
+        
+        <br></br>
+        <button type="button" onClick="Login()">Log in</button>
+        
+        </form>
       </header>
     </div>
   );
